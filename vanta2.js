ec3c63a197b6e72e800a7d4c6e6d120a6b14c4f0
riyaverma85 <riyaverma2976@gmail.com>
2025-05-13 21:23:32 -0700

    commit

---
+++ vanta2.js
@@ -0,0 +1,10 @@
+VANTA.CLOUDS2({
+  el: "#main",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  texturePath: "./gallery/noise.png"
+})
